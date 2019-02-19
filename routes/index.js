0bb1a7c80230ad32a47ca502ea869363cf29a39d
David Matheson <mathesond2@gmail.com>
2019-02-19 16:02:43 -0500

    removes passport deps, general cleaning

--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,44 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
-var GitHubStrategy = require('passport-github').Strategy;
-var clientID = process.env.GITHUB_CLIENT_ID;
-var clientSecret = process.env.GITHUB_CLIENT_SECRET;
-var github = require('octonode');
-var client;
-var ghorg;
-var myArr = [];
-
-
-passport.use(new GitHubStrategy({
-  clientID: clientID,
-  clientSecret: clientSecret,
-  callbackURL: "/login/github/callback"
-},
-  function (accessToken, refreshToken, profile, cb) {
-    // console.log(profile);
-    client = github.client(accessToken);
-    return cb(null, profile, accessToken);
-  }
-));
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
-passport.serializeUser(function (user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-  cb(null, obj);
-});
-
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-router.use(passport.initialize());
-router.use(passport.session());
+const express = require('express');
+const router = express.Router();
+const github = require('octonode');
+let client;
+let ghorg;
+let orgRepos = [];
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
-});
-
-router.get('/add', function (req, res, next) {
-  console.log('myArr', myArr);
-  res.render('add', { user: req.user, blah: myArr });
+  res.render('index', { user: req.user });
 });
 
 router.post('/registerOrg', function (req, res, next) {
+  client = github.client(req.body.token);
   ghorg = client.org(req.body.orgName);
-  ghorg.repos(function (err, data, headers) {
-    handleData(data);
+  ghorg.repos((err, data, headers) => {
+    handleUserData(data);
     res.redirect('/add');
   });
 
-  function handleData(data) {
+  function handleUserData(data) {
     data.forEach(function (obj) {
-      var newObj = {
-        name: obj.name,
-        url: obj.url
-      };
-      myArr.push(newObj);
+      var newObj = { name: obj.name };
+      orgRepos.push(newObj);
     });
-    return myArr;
+    return orgRepos;
   }
 });
 
-router.get('/login/github',
-  passport.authenticate('github'));
+router.get('/add', function (req, res, next) {
+  // console.log('orgRepos', orgRepos);
+  res.render('add', { user: req.user, orgRepos });
+});
 
-router.get('/login/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
-    res.redirect('/');
-  }
-);
+router.post('/add', function (req, res, next) {
+  const ghrepo = client.repo(`${ghorg.name}/${req.body.repo}`);
+  ghrepo.issue({
+    "title": req.body.title,
+    "body": req.body.description,
+  }, function () {
+    res.redirect('/add');
+  });
+});
 
 module.exports = router;
