dd090a6f5eb7fdc009c40f5ed6bc25da5de2acf5
David Matheson <mathesond2@gmail.com>
2019-03-06 16:06:34 -0500

    removes excess

--- routes/index.js
+++ routes/index.js
@@ -72,61 +72,16 @@ router.get('/updateAvailableRepos', function (req, res, next) {
 
 
 router.post('/updateAvailableRepos', function (req, res, next) {
-  // console.log('orgRepos', orgRepos);
-  // let availableRepos = [];
-  // let blah = JSON.parse(req.body.repos);
-  // let myArr = [];
-  // console.log('req.body.repos', req.body.repos);
-  // console.log('typeof  req.body.repos', typeof (req.body.repos));
   let availableRepos = typeof (req.body.repos) === 'object' ? req.body.repos : [req.body.repos];
-  // console.log('blah', availableRepos);
   orgRepos.forEach((obj, i) => {
     if (!availableRepos.includes(obj.name)) orgRepos.splice(i, 1);
   });
-  // console.log('orgRepos NOW', orgRepos);
-
-
-
-
-  // let blah = typeof (req.body.repos) === 'object' ? Object.keys(req.body.repos).map(i => req.body.repos[i]) : JSON.parse(req.body.repos);
-  // console.log('blah', blah);
-  // console.log(Object.values(blah));
-  // let ha = Object.values(blah).length < 2 ? blah.name : blah.name;
-  // console.log('ha', ha);
-  // const peopleArray = Object.keys(req.body.repos).map(i => req.body.repos[i]);
-  // const peopleArray = Object.keys(req.body.repos).map(i => req.body.repos[i]);
-  // console.log('peopleArray', peopleArray);
-  // console.log('typeof peopleArray', typeof (peopleArray));
-
-  // let myArr = [blah];
-  // let myArr = Object.keys(blah).map(i => blah[i]);
-  // console.log('myArr', myArr);
-
-  // myArr.forEach((obj, i) => {
-
-  // console.log('obj', obj);
-  //   console.log('typeof obj', typeof (obj));
-  //   console.log('name is', obj["name"]);
-  //   availableRepos.push(obj["name"]);
-  // });
-
-  // console.log('availableRepos', availableRepos);
   orgCredentials.availableRepos = availableRepos;
-  // orgRepos.forEach((obj, i) => {
-  //   // if (!currentOrgCredentials.availableRepos.includes(obj.name)) orgRepos.splice(i, 1);
-
-  // });
-
-
   fs.writeFile('orgCredentials.json', JSON.stringify(orgCredentials), 'utf8', function () { });
   res.redirect('/add');
 });
 
 router.get('/add', function (req, res, next) {
-  // console.log('availableRepos', availableRepos);
-  console.log('orgRepos', orgRepos);
-  console.log('currentOrgCredentials.availableRepos', currentOrgCredentials.availableRepos);
-
   if (orgRepos.length === 0) {
     client = github.client(currentOrgCredentials.token);
     ghorg = client.org(currentOrgCredentials.orgName);
@@ -135,9 +90,6 @@ router.get('/add', function (req, res, next) {
         console.log('ERROR: ', err)
       } else {
         handleUserData(data);
-        // orgRepos.forEach((obj, i) => {
-        //   if (!currentOrgCredentials.availableRepos.includes(obj.name)) orgRepos.splice(i, 1);
-        // });
         orgRepos.forEach((obj, i) => {
           if (currentOrgCredentials.availableRepos && !currentOrgCredentials.availableRepos.includes(obj.name)) orgRepos.splice(i, 1);
         });
@@ -145,11 +97,6 @@ router.get('/add', function (req, res, next) {
       }
     });
   } else {
-    // orgRepos.forEach((obj, i) => {
-    //   if (availableRepos.length && !availableRepos.includes(obj["name"])) {
-    //     orgRepos.splice(i, 1);
-    //   }
-    // });
     res.render('add', { orgRepos, flashMessage: req.flash(flashMessage) });
   }
 });
