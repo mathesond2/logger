678f22c874a0e190eacd3d39bdb81ddc039dbdf9
David Matheson <mathesond2@gmail.com>
2019-03-25 15:30:52 -0400

    adds 'javascripts' folder

---
+++ public/javascripts/user.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+let User = module.exports = {
+  client: null,
+  correctAccessToken: null,
+  orgCredentials: {
+    token: null,
+    orgName: null,
+  },
+  githubOrg: null,
+  orgRepos: [],
+  removeCredentials: () => {
+    User.orgCredentials = {};
+    fs.writeFile('orgCredentials.json', JSON.stringify(User.orgCredentials), 'utf8', function () { });
+  },
+  changeGithubOrg: (name) => {
+    User.githubOrg = User.client.org(name);
+  },
+  handleUserData: (data) => {
+    User.orgRepos = [];
+    data.forEach(function (obj) {
+      let newObj = {
+        name: obj.name,
+        description: obj.description
+      };
+      User.orgRepos.push(newObj);
+    });
+    return User.orgRepos;
+  },
+  filterUserData: () => {
+    let parsedData = JSON.parse(fs.readFileSync('./orgCredentials.json', 'utf8'));
+    User.orgRepos.forEach((obj, i) => {
+      if (parsedData.availableRepos &&
+        !parsedData.availableRepos.includes(obj.name)) User.orgRepos.splice(i, 1);
+    });
+    return User.orgRepos;
+  },
+};
