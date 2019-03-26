3b6a2799f257678d0a6b8294214de73791f9943b
David Matheson <mathesond2@gmail.com>
2019-03-26 14:33:56 -0400

    wip

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -5,34 +5,34 @@ const user1 = request.agent();
 const agent = require('supertest').agent;
 
 
-const createAuthenticatedUser = (done) => {
-  // const httpServer = app.listen();
-  const authenticatedUser = agent(app);
-  console.log('authenticatedUser', authenticatedUser);
-  authenticatedUser
-    .get('/login/github/callback')
-    .end((error, resp) => {
-      done(authenticatedUser);
-      // httpServer.close();
-    });
-}
+// const createAuthenticatedUser = (done) => {
+//   // const httpServer = app.listen();
+//   const authenticatedUser = agent(app);
+//   console.log('authenticatedUser', authenticatedUser);
+//   authenticatedUser
+//     .get('/login/github/callback')
+//     .end((error, resp) => {
+//       done(authenticatedUser);
+//       // httpServer.close();
+//     });
+// }
 
-describe('GET /select-repos', () => {
-  test('it should work', () => {
-    createAuthenticatedUser(async (request) => {
-      const response = await request(app).get('/blah-repos');
-      console.log('response.statusCode', response.statusCode);
-      expect(response.statusCode).toBe(404);
-      // request
-      //   .get('/blah-repos')
-      //   .expect(200)
-      //   .end((err, res) => {
-      //     console.log(res.body);
-      //     // t.end(err);
-      //   });
-    });
-  });
-});
+// describe('GET /select-repos', () => {
+//   test('it should work', () => {
+//     createAuthenticatedUser(async (request) => {
+//       const response = await request(app).get('/blah-repos');
+//       console.log('response.statusCode', response.statusCode);
+//       expect(response.statusCode).toBe(404);
+//       // request
+//       //   .get('/blah-repos')
+//       //   .expect(200)
+//       //   .end((err, res) => {
+//       //     console.log(res.body);
+//       //     // t.end(err);
+//       //   });
+//     });
+//   });
+// });
 
 
 // user1
@@ -66,32 +66,61 @@ describe('GET /', () => {
   });
 });
 
-describe('GET /select-repos without auth', () => {
+describe('POST /register-org', () => {
+  test('302 should be returned', async () => {
+    const response = await request(app).post('/register-org');
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+/* 
+3. 'POST /registerOrg' request (must do auth beforehand)..
+  * should return status code for success
+  * should return data
+*/
+
+/* 
+4. 'GET /update-repos' request (must do auth beforehand)
+  * should return status code for success
+  * should return data
+*/
+
+/** 
+ * 
+ * POST update-repos
+*/
+
+describe('GET /add-issue without auth', () => {
   test('non-authed routes should redirect', async () => {
-
-
-    // var user1 = request.agent(app);
-    // user1.getCookies();
-    // console.log('user1', user1);
-    // user1
-    //   .get('http://localhost:3000/login/github')
-    //   // .send({ user: 'hunter@hunterloftis.com', password: 'password' })
-    //   .end(function (err, res) {
-    //     console.log('res is', res);
-    //     // user1 will manage its own cookies
-    //     // res.redirects contains an Array of redirects
-    //   });
-
-
-
-    const response = await request(app).get('/select-repos');
+    const response = await request(app).get('/add-issue');
     expect(response.statusCode).toBe(302);
   });
 });
 
-// describe('GET /select-repos', () => {
-//   test('non-authed routes should redirect', async () => {
-//     const response = await request(app).get('/select-repos');
-//     expect(response.statusCode).toBe(302);
-//   });
-// });
+// GET /add-issue with auth
+
+/*
+6. 'POST /add-issue' request (must do auth beforehand)
+  * should return status code for success
+  * should return data
+ */
+
+describe('GET /reset-credentials', () => {
+  test('credentials should be removed', async () => {
+    const response = await request(app).get('/reset-credentials');
+    let parsedData = JSON.parse(fs.readFileSync('./orgCredentials.json', 'utf8'));
+    expect(Object.keys(parsedData).length).toEqual(0);
+  });
+
+  test('re-direct should occur', async () => {
+    const response = await request(app).get('/reset-credentials');
+    expect(response.statusCode).toBe(302);
+  });
+});
+
+describe('GET /settings', () => {
+  test('settings view should be rendered', async () => {
+    const response = await request(app).get('/settings').type('text/html');
+    expect(response.text).toBeDefined();
+  });
+});
