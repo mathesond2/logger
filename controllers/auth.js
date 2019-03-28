1640782e1d592d6f2da57e2d821b1a2bc06e2f16
David Matheson <mathesond2@gmail.com>
2019-03-28 12:56:17 -0400

    fixes routing issue & blocks DB insertion

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 //strategy = something will interface with checking if you're allowed to login.
 exports.login = passport.authenticate('local', {
-  failureRedirect: '/sign-up',
+  failureRedirect: '/',
   failureFlash: 'Failed Login!',
   successRedirect: '/register-org',
   successFlash: 'you are logged in! 🎉',
