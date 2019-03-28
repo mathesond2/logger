622d612a6f22987aa4d9615a621a6379839a066d
David Matheson <mathesond2@gmail.com>
2019-03-28 12:59:16 -0400

    adds proper path

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 //strategy = something will interface with checking if you're allowed to login.
 exports.login = passport.authenticate('local', {
-  failureRedirect: '/',
+  failureRedirect: '/login',
   failureFlash: 'Failed Login!',
   successRedirect: '/register-org',
   successFlash: 'you are logged in! 🎉',
