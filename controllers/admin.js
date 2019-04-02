97644081e326eab76d45bcc6f44743a34f1cbee8
David Matheson <mathesond2@gmail.com>
2019-04-02 12:26:46 -0400

    adds deps

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,9 @@ const currentOrgCredentials = require("./../orgCredentials.json");
 const user = require("../public/javascripts/user");
 const fs = require("fs");
 const github = require('octonode');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.resetCredentials = (req, res) => {
   user.removeCredentials();
